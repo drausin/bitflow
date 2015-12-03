2ef6eb2f120161193d467c19a7bf9bac8410d726
Drausin Wulsin <daedalus2718@gmail.com>
2015-12-03 20:09:55 +0900

    add BigInteger SerDes

---
+++ bitflow-bitcoin/src/test/java/org/drausin/bitflow/blockchain/serde/BigIntegerDeserializerTest.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain.serde;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.fasterxml.jackson.core.JsonParser;
+import java.math.BigInteger;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class BigIntegerDeserializerTest {
+
+    private BigIntegerDeserializer bigIntegerDeserializer;
+
+    @Mock
+    private JsonParser parser;
+
+    @Before
+    public final void setUp() throws Exception {
+        bigIntegerDeserializer = new BigIntegerDeserializer();
+        parser = mock(JsonParser.class);
+    }
+
+    @Test
+    public final void testDeserialize() throws Exception {
+        when(parser.getValueAsString()).thenReturn("100");
+        assertEquals(new BigInteger("256", 10), bigIntegerDeserializer.deserialize(parser, null));
+    }
+
+    @Test
+    public final void testHandledType() throws Exception {
+        assertEquals(BigInteger.class, bigIntegerDeserializer.handledType());
+    }
+}
---
+++ bitflow-bitcoin/src/test/java/org/drausin/bitflow/blockchain/serde/BigIntegerSerializerTest.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain.serde;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.math.BigInteger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BigIntegerSerializerTest {
+
+    private BigIntegerSerializer bigIntegerSerializer;
+
+    @Mock
+    private JsonGenerator gen;
+
+    @Before
+    public final void setUp() throws Exception {
+        bigIntegerSerializer = new BigIntegerSerializer();
+    }
+
+    @Test
+    public final void testSerialize() throws Exception {
+        bigIntegerSerializer.serialize(new BigInteger("256", 10), gen, null);
+        verify(gen, times(1)).writeString("100");
+    }
+
+    @Test
+    public final void testHandledType() throws Exception {
+        assertEquals(BigInteger.class, bigIntegerSerializer.handledType());
+    }
+}
