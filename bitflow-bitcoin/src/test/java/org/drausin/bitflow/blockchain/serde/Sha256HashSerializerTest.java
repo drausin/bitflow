d5740539d4a8106e50aecdfb5536307dfda1fe0e
Drausin Wulsin <daedalus2718@gmail.com>
2015-12-03 20:20:51 +0900

    add Sha256HashSerializer

---
+++ bitflow-bitcoin/src/test/java/org/drausin/bitflow/blockchain/serde/Sha256HashSerializerTest.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain.serde;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import java.math.BigInteger;
+import org.bitcoinj.core.Sha256Hash;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Sha256HashSerializerTest {
+
+    private Sha256HashSerializer sha256HashSerializer;
+
+    @Mock
+    private JsonGenerator gen;
+
+    @Before
+    public final void setUp() throws Exception {
+        sha256HashSerializer = new Sha256HashSerializer();
+    }
+
+    @Test
+    public final void testSerialize() throws Exception {
+        String hexHash = "000000000ebb17fb455e897b8f3e343eea1b07d926476d00bc66e2c0342ed50f";
+        sha256HashSerializer.serialize(Sha256Hash.wrap(hexHash), gen, null);
+        verify(gen, times(1)).writeString(hexHash);
+    }
+
+    @Test
+    public final void testHandledType() throws Exception {
+        assertEquals(Sha256Hash.class, sha256HashSerializer.handledType());
+    }
+}
