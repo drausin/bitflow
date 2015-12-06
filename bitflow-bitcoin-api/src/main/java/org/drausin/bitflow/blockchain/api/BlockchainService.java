1a3b83cf98ed8813e0a85401be6d684788347920
Drausin Wulsin <daedalus2718@gmail.com>
2015-12-06 15:25:11 +0900

    add bitcoin-api module with BlockchainService interface

---
+++ bitflow-bitcoin-api/src/main/java/org/drausin/bitflow/blockchain/api/BlockchainService.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain.api;
+
+import com.google.common.base.Optional;
+import java.util.Map;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import org.drausin.bitflow.blockchain.BlockchainInfo;
+
+@Path("/blockchain")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface BlockchainService {
+
+    /**
+     * Gets current information about the blockchain.
+     */
+    @GET
+    @Path("/info")
+    @Produces(MediaType.APPLICATION_JSON)
+    BlockchainInfo getBlockchainInfo(
+            @HeaderParam(HttpHeaders.AUTHORIZATION) String authHeader);
+
+
+    /**
+     * Gets the block header for a given block hash.
+     *
+     * @param hash the hash of the block
+     */
+    @GET
+    @Path("/block/header/{hash}")
+    @Produces(MediaType.APPLICATION_JSON)
+    Optional<Map<String, Object>> getBlockHeader(
+            @HeaderParam(HttpHeaders.AUTHORIZATION) String authHeader,
+            @PathParam("hash") String hash);
+
+    // TODO: getBlockTimeSubchain(long fromTime, long toTime)
+
+    // TODO: getBlockTimeSubchain(Date fromDate, Date toDate)
+
+    // TODO: getBlockHeightSubchain(long fromHeight, long toHeight)
+}
