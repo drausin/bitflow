eedbab86a008d07a5a660c8f13d6ea1021374b7b
Drausin Wulsin <daedalus2718@gmail.com>
2015-12-07 12:26:24 +0900

    rename bitflow-bitcoin/*.blockchain --> *.blockchain.api

--- bitflow-bitcoin-api/src/main/java/org/drausin/bitflow/blockchain/api/api/BlockchainService.java
+++ bitflow-bitcoin-api/src/main/java/org/drausin/bitflow/blockchain/api/api/BlockchainService.java
@@ -12,7 +12,21 @@
  * limitations under the License.
  */
 
-package org.drausin.bitflow.blockchain.api;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain.api.api;
 
 import java.util.List;
 import javax.annotation.CheckForNull;
@@ -25,8 +39,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
-import org.drausin.bitflow.blockchain.BlockHeader;
-import org.drausin.bitflow.blockchain.BlockchainInfo;
+import org.drausin.bitflow.blockchain.api.BlockHeader;
+import org.drausin.bitflow.blockchain.api.BlockchainInfo;
 import org.joda.time.DateTime;
 
 @Path("/blockchain")
