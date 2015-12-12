c6d90acf69928fccbe91005a26e3c57d14b8bb2a
Drausin Wulsin <daedalus2718@gmail.com>
2015-12-12 18:23:16 -0500

    add class for bitcoind RPC service config

---
+++ bitflow-bitcoin/src/main/java/org/drausin/bitflow/blockchain/client/config/BitcoindRpcServiceConfig.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain.client.config;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.apache.commons.lang3.Validate;
+
+/**
+ * RPC service configuation.
+ *
+ * @author dwulsin
+ */
+public final class BitcoindRpcServiceConfig {
+
+    private final String uri;
+    private final String rpcUser;
+    private final String rpcPassword;
+
+    public BitcoindRpcServiceConfig(
+            @JsonProperty String uri,
+            @JsonProperty String rpcUser,
+            @JsonProperty String rpcPassword) {
+        this.uri = Validate.notEmpty(uri, "Must specify an RPC service URI");
+        this.rpcUser = Validate.notEmpty(rpcUser, "Must specify a non-empty RPC user");
+        this.rpcPassword = rpcPassword;
+    }
+
+    public String getUri() { return uri; }
+
+    public String getRpcUser() { return rpcUser; }
+
+    public String getRpcPassword() { return rpcPassword; }
+}
---
+++ bitflow-bitcoin/src/test/java/org/drausin/bitflow/blockchain/client/config/BitcoindRpcServiceConfigTest.java
@@ -0,0 +1,53 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain.client.config;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BitcoindRpcServiceConfigTest {
+
+    private BitcoindRpcServiceConfig config;
+    private String uri;
+    private String rpcUser;
+    private String rpcPassword;
+
+    @Before
+    public void setUp() throws Exception {
+
+        uri = "http://localhost:0000";
+        rpcUser = "auser";
+        rpcPassword = "apassword";
+
+        config = new BitcoindRpcServiceConfig(uri, rpcUser, rpcPassword);
+    }
+
+    @Test
+    public void testGetUri() throws Exception {
+        assertEquals(uri, config.getUri());
+    }
+
+    @Test
+    public void testGetRpcUser() throws Exception {
+        assertEquals(rpcUser, config.getRpcUser());
+    }
+
+    @Test
+    public void testGetRpcPassword() throws Exception {
+        assertEquals(rpcPassword, config.getRpcPassword());
+    }
+}
