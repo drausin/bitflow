ee13d1d12623098ebc576b06f302d23f489c5db2
Drausin Wulsin <daedalus2718@gmail.com>
2015-12-25 21:13:04 -0700

    add test for BlockchainResource

---
+++ bitflow-bitcoin/src/test/java/org/drausin/bitflow/blockchain/BlockchainResourceTest.java
@@ -0,0 +1,57 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drausin.bitflow.blockchain;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.bitcoinj.core.Sha256Hash;
+import org.drausin.bitflow.blockchain.client.BitcoindRpcService;
+import org.drausin.bitflow.blockchain.client.objects.BitcoindRpcExampleResponses;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlockchainResourceTest {
+
+    private BlockchainResource blockchainResource;
+
+    @Before
+    public void setUp() throws Exception {
+        BitcoindRpcService bitcoindRpcService = mock(BitcoindRpcService.class);
+
+        when(bitcoindRpcService.getBlockchainInfo()).thenReturn(BitcoindRpcExampleResponses.getBlockchainInfoResult());
+        when(bitcoindRpcService.getBlockHeader(any(Sha256Hash.class)))
+                .thenReturn(BitcoindRpcExampleResponses.getBlockHeaderResult());
+
+        blockchainResource = new BlockchainResource(bitcoindRpcService);
+    }
+
+    @Test
+    public void testGetBlockchainInfo() throws Exception {
+        assertThat(
+                blockchainResource.getBlockchainInfo("dummy auth header"),
+                is(BitcoindRpcExampleResponses.getBlockchainInfoResult()));
+    }
+
+    @Test
+    public void testGetBlockHeader() throws Exception {
+        assertThat(
+                blockchainResource.getBlockHeader("dummy auth header", any(Sha256Hash.class)),
+                is(BitcoindRpcExampleResponses.getBlockHeaderResult()));
+    }
+}
