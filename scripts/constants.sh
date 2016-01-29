93f10f37a7609ddb11f80055732a19d053cabf24
Drausin Wulsin <daedalus2718@gmail.com>
2016-01-28 21:22:06 -0500

    add docker-setup script

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -3,3 +3,6 @@
 if [ -n "${JAVA_1_8_HOME}" ]; then export JAVA_HOME=$JAVA_1_8_HOME; fi
 GRADLE_PARALLEL="--parallel --max-workers 8"
 GRADLE_LOG_LEVEL="--info --stacktrace"
+
+# our docker machine
+DOCKER_MACHINE_NAME=bitflow-docker
