596cc072a6d2fffc4c1a8872d08dc47554cc4c77
Drausin Wulsin <daedalus2718@gmail.com>
2016-01-31 18:40:52 -0500

    set max gradle parallelism to 4 and mem to 2G

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export JAVA_HOME=${JAVA_1_8_HOME:-${JAVA_HOME}}
-
-GRADLE_PARALLEL="--parallel --max-workers 8"
+export GRADLE_OPTS=-Xmx2048m
+GRADLE_PARALLEL="--parallel --max-workers 4"
 GRADLE_LOG_LEVEL="--info --stacktrace"
 
 # our docker machine
