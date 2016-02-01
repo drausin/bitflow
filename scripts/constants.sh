22e80dfee315955412aa2ff9b898c465df59773e
Drausin Wulsin <daedalus2718@gmail.com>
2016-01-31 20:38:14 -0500

    limit to 1GB mem and have only 3 workers

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export JAVA_HOME=${JAVA_1_8_HOME:-${JAVA_HOME}}
-export GRADLE_OPTS=-Xmx2048m
-GRADLE_PARALLEL="--parallel --max-workers 4"
+export GRADLE_OPTS=-Xmx1024m
+GRADLE_PARALLEL="--parallel --max-workers 3"
 GRADLE_LOG_LEVEL="--info --stacktrace"
 
 # our docker machine
